8714c5f565a103d7634eed5f79d35b9fc574f83a
Agata Mosinska <mosinskaagata@gmail.com>
2023-07-04 22:47:46 +0200

    add more connectivity measures

--- plv.py
+++ plv.py
@@ -11,15 +11,14 @@ from hypyp import utils
 from copy import copy
 
 
-class PLV:
-
+class connectivityMeasure:
     '''
-    Computes PLV for one dyad
+    Computes a functional connectivity measure for one dyad
     '''
-
-    def __init__(self, baby_epochs, mom_epochs):
+    def __init__(self, baby_epochs, mom_epochs, mode):
         self.babyEpochs = baby_epochs
         self.momEpochs = mom_epochs
+        self.mode = mode # plv, pli, wpli
         self.data = np.array([baby_epochs.get_data(), mom_epochs.get_data()])
         self.freq_bands = {'Theta-Baby': [3, 5],
                            'Theta-Mom': [4, 7],
@@ -28,25 +27,32 @@ class PLV:
         self.sampling_rate = self.babyEpochs.info["sfreq"]
 
 
-    def get_plv(self):
-        '''
-        Method calculating the single frequency PLV 
-        '''
+    def calculate_sync(self):
         complex_signal = analyses.compute_freq_bands(self.data, self.sampling_rate, self.freq_bands)
-        self.plv = analyses.compute_sync(complex_signal, mode='plv', epochs_average=False)
+        if self.mode == 'plv':
+            self.plv = analyses.compute_sync(complex_signal, mode='plv', epochs_average=False)
+            sync_data = self.plv
+        elif self.mode == 'pli':
+            self.pli = analyses.compute_sync(complex_signal, mode='pli', epochs_average=False)
+            sync_data = self.pli
+        elif self.mode == 'wpli':
+            self.wpli = analyses.compute_sync(complex_signal, mode='wpli', epochs_average=False)
+            sync_data = self.wpli
+        else:
+            raise ValueError("Invalid mode provided")
+
         n_ch = len(self.babyEpochs.info['ch_names'])
-        self.theta_baby, self.theta_mom, self.alpha_baby, self.alpha_mom = self.plv[:, 0:n_ch, n_ch:2*n_ch]        
+        self.theta_baby, self.theta_mom, self.alpha_baby, self.alpha_mom = sync_data[:, 0:n_ch, n_ch:2*n_ch]
 
 
-class pseudoPLV:
-
+class pseudoConnectivityMeasure:
     '''
     Computes PLV for one dyad shuffling the Hilbert transform to introduce randomization
     '''
-
-    def __init__(self, baby_epochs, mom_epochs):
+    def __init__(self, baby_epochs, mom_epochs, mode):
         self.babyEpochs = baby_epochs
         self.momEpochs = mom_epochs
+        self.mode = mode
         self.data = np.array([np.array(baby_epochs), np.array(mom_epochs)])
         self.freq_bands = {'Theta-Baby': [3, 5],
                            'Theta-Mom': [4, 7],
@@ -55,9 +61,9 @@ class pseudoPLV:
         self.sampling_rate = self.babyEpochs.info["sfreq"]
 
 
-    def get_plv(self):
+    def calculate_surrogate_sync(self):
         '''
-        Method calculating the single frequency PLV with shuffling of the Hilbert transform
+        Method calculating the single frequency connectivity measure with shuffling of the Hilbert transform
         The code was adapted from the HyPyP library code
         '''
         # filtering and hilbert transform
@@ -75,7 +81,17 @@ class pseudoPLV:
 
         complex_signal = np.moveaxis(np.array(complex_signal), [0], [3])
 
-        # complex_signal = analyses.compute_freq_bands(self.data, self.sampling_rate, self.freq_bands)
-        self.plv = analyses.compute_sync(complex_signal, mode='plv', epochs_average=False)
+        if self.mode == 'plv':
+            self.plv = analyses.compute_sync(complex_signal, mode='plv', epochs_average=False)
+            sync_data = self.plv
+        elif self.mode == 'pli':
+            self.pli = analyses.compute_sync(complex_signal, mode='pli', epochs_average=False)
+            sync_data = self.pli
+        elif self.mode == 'wpli':
+            self.wpli = analyses.compute_sync(complex_signal, mode='wpli', epochs_average=False)
+            sync_data = self.wpli
+        else:
+            raise ValueError("Invalid mode provided")
+
         n_ch = len(self.babyEpochs.info['ch_names'])
-        self.theta_baby, self.theta_mom, self.alpha_baby, self.alpha_mom = self.plv[:, 0:n_ch, n_ch:2*n_ch]
+        self.theta_baby, self.theta_mom, self.alpha_baby, self.alpha_mom = sync_data[:, 0:n_ch, n_ch:2*n_ch]
--- validation_script.py
+++ validation_script.py
@@ -1,98 +1,114 @@
 from load_data import DataLoader, Infant, Mom
-from plv import PLV, pseudoPLV
+from plv import connectivityMeasure, pseudoConnectivityMeasure
 import os
 import mne
 import numpy as np
 import scipy.signal as sig
 import json
 import re
+from copy import copy
 
+
+def validate_synchrony(dataPath, mode):
+    connectivity_results_theta_all = {} # non-validated results for theta frequency band 
+    connectivity_results_alpha_all = {} # non-validated results for alpha frequency band 
+    connectivity_results_theta = {}  # validated results for theta frequency band 
+    connectivity_results_alpha = {}  # validated results for alpha frequency band
+
+    # Loop over each stage (subfolder) within the participant's path
+    for participant in sorted(os.listdir(dataPath)):
+        participantPath = os.path.join(dataPath, participant)
+        participant_idx = re.findall(r'\d+', str(participantPath)[14:])[0]
+
+        stage_dict_theta_all = {} # vaildated results for theta frequency at each stage
+        stage_dict_alpha_all = {} #  validated results for alpha frequency at each stage
+        stage_dict_theta = {} # vaildated results for theta frequency at each stage
+        stage_dict_alpha = {} #  validated results for alpha frequency at each stage
+
+        for sfp_stage in sorted(os.listdir(participantPath)):
+            sfp_stagePath = os.path.join(participantPath, sfp_stage)
+            stage = re.findall('-[0-5]-', str(sfp_stagePath))[0]
+
+            dyad = DataLoader(sfp_stagePath)
+            dyad.read_data()
+
+            baby = Infant(dyad.infant_path)
+            baby.read_data()
+
+            mom = Mom(dyad.mother_path)
+            mom.read_data()
+
+            # Drop channels that are not present in both infant and mother data
+            for chan_baby in baby.epochs.info['ch_names']:
+                if chan_baby not in mom.epochs.info['ch_names']:
+                    baby.epochs.drop_channels(chan_baby)
+            for chan_mom in mom.epochs.info['ch_names']:
+                if chan_mom not in baby.epochs.info['ch_names']:
+                    mom.epochs.drop_channels(chan_mom)
+
+            # Compute synchrony
+            connectivity = connectivityMeasure(baby.epochs, mom.epochs, mode)
+            connectivity.calculate_sync() 
+            connectivity_theta = connectivity.theta_baby.copy() # n_segments x n_channels x n_channels 
+            connectivity_alpha = connectivity.alpha_baby.copy()
+
+            # Array to store counts of real sync > surr sync for theta frequenc
+            compared_theta = np.zeros_like(connectivity_theta)
+            compared_alpha = np.zeros_like(connectivity_alpha)
+
+            # Calculate surrogate connectivity measures
+            n_surrogates = 1
+            for surr in range(n_surrogates):
+                # compute surrogate connectivity measures 
+                surr_connectivity = pseudoConnectivityMeasure(baby.epochs, mom.epochs, mode)
+                surr_connectivity.calculate_surrogate_sync()
+
+                # theta comparison
+                surr_theta = surr_connectivity.theta_baby
+                mask_theta = connectivity_theta > surr_theta
+                compared_theta[mask_theta] += 1
+
+                # alpha comparison
+                surr_alpha = surr_connectivity.alpha_baby
+                mask_alpha = connectivity_alpha > surr_alpha
+                compared_alpha[mask_alpha] += 1
+
+            threshold = np.full_like(connectivity_theta, 0.95*n_surrogates) # Calculate the threshold for significant PLV
+
+            # Create a mask for significant PLV in each frequency band
+            mask_t = compared_theta > threshold
+            mask_a = compared_alpha > threshold
+
+            # Set non-significant theta PLV values to NaN
+            connectivity_theta = np.where(mask_t, connectivity_theta, np.nan)
+            connectivity_alpha = np.where(mask_a, connectivity_alpha, np.nan)
+
+            # Convert PLV arrays to lists and store in stage_dict_theta/alpha
+            stage_dict_theta_all[stage] = connectivity.theta_baby.tolist()
+            stage_dict_alpha_all[stage] = connectivity.alpha_baby.tolist()
+            stage_dict_theta[stage] = connectivity_theta.tolist()
+            stage_dict_alpha[stage] = connectivity_alpha.tolist()
+
+        connectivity_results_theta_all[participant_idx] = stage_dict_theta_all 
+        connectivity_results_alpha_all[participant_idx] = stage_dict_alpha_all
+        connectivity_results_theta[participant_idx] = stage_dict_theta 
+        connectivity_results_alpha[participant_idx] = stage_dict_alpha
+
+    with open(f"nonvalidated_results_theta_{mode}.json", "w") as results_file:
+        json.dump(connectivity_results_theta_all, results_file)
+        
+    with open(f"nonvalidated_results_alpha_{mode}.json", "w") as results_file:
+        json.dump(connectivity_results_alpha_all, results_file)
+
+    with open(f"validated_results_theta_{mode}.json", "w") as results_file:
+        json.dump(connectivity_results_theta, results_file)
+        
+    with open(f"validated_results_alpha_{mode}.json", "w") as results_file:
+        json.dump(connectivity_results_alpha, results_file)
+
+    
 # path to the folder with all participants
-dataPath = "/home/u692590/thesis/dyad_data/preprocessed_data"
-# dataPath = "/home/agata/Desktop/thesis/dyad_data/preprocessed_data/"
+gpu_path = "/home/u692590/thesis/dyad_data/preprocessed_data"
+# local_path = "/home/agata/Desktop/thesis/dyad_data/preprocessed_data/"
 
-plv_results_theta = {}  # Dictionary to store PLV results for theta frequency band 
-plv_results_alpha = {}  # Dictionary to store PLV results for alpha frequency band
-
-# Loop over each stage (subfolder) within the participant's path
-for participant in sorted(os.listdir(dataPath)):
-    participantPath = os.path.join(dataPath, participant)
-    participant_idx = re.findall(r'\d+', str(participantPath)[14:])[0]
-
-    stage_dict_theta = {} # Dictionary to store PLV results for theta frequency at each stage
-    stage_dict_alpha = {} #  Dictionary to store PLV results for alpha frequency at each stage
-
-    for sfp_stage in sorted(os.listdir(participantPath)):
-        sfp_stagePath = os.path.join(participantPath, sfp_stage)
-        stage = re.findall('-[0-5]-', str(sfp_stagePath))[0]
-
-        dyad = DataLoader(sfp_stagePath)
-        dyad.read_data()
-
-        baby = Infant(dyad.infant_path)
-        baby.read_data()
-
-        mom = Mom(dyad.mother_path)
-        mom.read_data()
-
-        # Drop channels that are not present in both infant and mother data
-        for chan_baby in baby.epochs.info['ch_names']:
-            if chan_baby not in mom.epochs.info['ch_names']:
-                baby.epochs.drop_channels(chan_baby)
-        for chan_mom in mom.epochs.info['ch_names']:
-            if chan_mom not in baby.epochs.info['ch_names']:
-                mom.epochs.drop_channels(chan_mom)
-
-        # Calculate the PLV
-        plv=PLV(baby.epochs, mom.epochs)
-        plv.get_plv() 
-        plv_theta = plv.theta_baby # n_segments x n_channels x n_channels 
-        plv_alpha = plv.alpha_baby
-
-        # Array to store counts of real PLV > surr PLV for theta frequenc
-        compared_theta = np.zeros_like(plv_theta)
-        compared_alpha = np.zeros_like(plv_alpha)
-
-        # Calculate surrogate PLVs
-        n_surrogates = 200
-        for surr in range(n_surrogates):
-            # compute surrogate PLVs
-            surrPLV = pseudoPLV(baby.epochs, mom.epochs)
-            surrPLV.get_plv()
-
-            # theta comparison
-            surr_theta = surrPLV.theta_baby
-            mask_theta = plv_theta > surr_theta
-            compared_theta[mask_theta] += 1
-
-            # alpha comparison
-            surr_alpha = surrPLV.alpha_baby
-            mask_alpha = plv_alpha > surr_alpha
-            compared_alpha[mask_alpha] += 1
-
-        threshold = np.full_like(plv_theta, 0.95*n_surrogates) # Calculate the threshold for significant PLV
-
-        # Create a mask for significant PLV in each frequency band
-        mask_t = compared_theta > threshold
-        mask_a = compared_alpha > threshold
-
-        # Set non-significant theta PLV values to NaN
-        plv_theta = np.where(mask_t, plv_theta, np.nan)
-        plv_alpha = np.where(mask_a, plv_alpha, np.nan)
-
-
-        # Convert PLV arrays to lists and store in stage_dict_theta/alpha
-        stage_dict_theta[stage] = plv_theta.tolist()
-        stage_dict_alpha[stage] = plv_alpha.tolist()
-
-    plv_results_theta[participant_idx] = stage_dict_theta 
-    plv_results_alpha[participant_idx] = stage_dict_alpha
-
-
-with open("results_theta_plv.json", "w") as results_file:
-    json.dump(plv_results_theta, results_file)
-    
-with open("results_alpha_plv.json", "w") as results_file:
-    json.dump(plv_results_alpha, results_file)
-
-    
+validate_synchrony(dataPath=gpu_path, mode="pli")
